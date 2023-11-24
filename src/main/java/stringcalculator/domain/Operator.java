fa740a4a97f36877d704cb826cb18aca5541aff6
hyeongiii <hyeongiii0811@gmail.com>
2023-11-24 11:35:01 +0900

    refactor: enum에 BiFunction을 멤버변수로 추가 (step1)

---
+++ src/main/java/stringcalculator/domain/Operator.java
@@ -0,0 +1,34 @@
+package stringcalculator.domain;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+public enum Operator {
+    ADD("+", (a, b) -> a + b),
+    SUB("-", (a, b) -> a - b),
+    MUL("*", (a, b) -> a * b),
+    DIV("/", (a, b) -> a / b),
+    UNKNOWN(null, null);
+
+    private final String operator;
+    private final BiFunction<Integer, Integer, Integer> calculateFunc;
+
+    Operator(String operator, BiFunction<Integer, Integer, Integer> calculateFunc) {
+        this.operator = operator;
+        this.calculateFunc = calculateFunc;
+    }
+
+    public static Operator findOperator(String symbol) {
+        return Arrays.stream(Operator.values())
+                .filter(type -> type.operator != null)
+                .filter(type -> symbol.equals(type.operator))
+                .findFirst()
+                .orElse(Operator.UNKNOWN);
+    }
+
+    public Integer calculate(Integer num1, Integer num2) {
+        assert this != UNKNOWN;
+        return this.calculateFunc.apply(num1, num2);
+    }
+
+}
